0c00172f1ae39ef7cf778fa0a30796d4c89c1d5e
vivek <viveksoftdevm@gmail.com>
2024-06-27 15:26:06 +0530

    created utilities in request_utils.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import asyncio
-import aiohttp
-
-from bs4 import BeautifulSoup
+from utils.request_utils import main
 
 page_count = 416
 url_list = [f'https://www.carwale.com/used/all-used-cars/page-{page}/'for page in range(2,page_count)]
 
-print(url_list)
+
+print(asyncio.run(main(url_list)))
---
+++ utils/request_utils.py
@@ -0,0 +1,15 @@
+import asyncio
+import aiohttp
+
+
+
+async def fetch_page(url,headers):
+    #establishing the session
+    async with aiohttp.ClientSession as session:
+        async with session.get(url,headers=headers) as response:
+            return await response.text()
+
+
+async def main(url_list):
+    #gathering all the results at once
+    return await asyncio.gather(*(fetch_page(url)for url in url_list))
