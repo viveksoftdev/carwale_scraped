0227ae5919382a26fe785887be507ce7c5750572
vivek <viveksoftdevm@gmail.com>
2024-06-27 17:09:37 +0530

    regular commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from utils.request_utils import main
+from utils.request_utils import (main,parse_return,
+                                 extract,to_csv)
 import asyncio
+import json
+import pandas as pd
 
 page_count = 416
 url_list = [f'https://www.carwale.com/used/all-used-cars/page-{page}/'for page in range(2,page_count)]
@@ -8,4 +11,17 @@ url_list = [f'https://www.carwale.com/used/all-used-cars/page-{page}/'for page i
 headers =  {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
 
 
-print(asyncio.run(main(url_list,headers=headers)))
+results = asyncio.run(main(url_list,headers=headers))
+
+parsed_list = parse_return(results)
+
+pattern = r'"url":\s{1,5}"([^,].+)?"'
+
+url_dict = extract(parsed_list=parsed_list,pattern=pattern,tag='script',num=0)
+
+#creating a dataframe out of the scraped urls
+file_name = 'carwale_urls.csv'
+to_csv(url_dict,file_name,)
+
+
+
--- utils/request_utils.py
+++ utils/request_utils.py
@@ -1,7 +1,8 @@
 import asyncio
 import aiohttp
-
-
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
 
 async def fetch_page(url,headers):
     #establishing the session
@@ -13,3 +14,30 @@ async def fetch_page(url,headers):
 async def main(url_list,headers):
     #gathering all the results at once
     return await asyncio.gather(*(fetch_page(url,headers)for url in url_list))
+
+
+def create_soup(data):
+    soup = BeautifulSoup(data,'html.parser')
+    return soup
+
+def parse_return(results):
+    parsed_list = []
+    for data in results:
+        parsed_list.append(create_soup(data))
+    return parsed_list
+
+def extract(parsed_list,pattern,tag,num):
+    import re
+    from collections import defaultdict
+    url_dict = defaultdict(list)
+    for soup in parsed_list:
+        print(soup.find_all(tag[num]))
+        for item in soup:
+            for _ in re.findall(pattern,str(item)):
+                url_dict['url'].append(_)
+    return url_dict
+
+def to_csv(url_dict,f_name,encoding='utf-8-sig'):
+    df = pd.DataFrame(url_dict)
+
+    df.to_csv('urls.csv',encoding='utf-8-sig')
